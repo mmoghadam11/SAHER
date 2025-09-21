eef236193786bd7eb76f52f896b4a990b2a1e047
mahdiyar_IACPA <mahyar_moghadam11@yahoo.ca>
2025-09-21 16:29:01 +0330

    tooltips -added

--- src/components/table/TableActions.tsx
+++ src/components/table/TableActions.tsx
@@ -1,32 +1,50 @@
 import React from "react";
-import { Box, IconButton } from "@mui/material";
+import { Box, IconButton, Tooltip } from "@mui/material";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import { AddCircleOutline } from "@mui/icons-material";
 
 type Props = {
   onEdit?: (record: any) => void;
   onDelete?: (record: any) => void;
   onView?: (record: any) => void;
+  onAdd?: {
+    function: (record: any) => void;
+    title: string;
+  };
 };
 
-const TableActions: React.FC<Props> = ({ onEdit, onDelete, onView }) => {
+const TableActions: React.FC<Props> = ({ onEdit, onDelete, onView, onAdd }) => {
   return (
     <Box>
       {onView && (
-        <IconButton onClick={onView} color="primary">
-          <VisibilityOutlinedIcon />
-        </IconButton>
+        <Tooltip title="مشاهده">
+          <IconButton onClick={onView} color="primary">
+            <VisibilityOutlinedIcon />
+          </IconButton>
+        </Tooltip>
+      )}
+      {onAdd && (
+        <Tooltip title={onAdd.title}>
+          <IconButton onClick={onAdd.function} color="primary">
+            <AddCircleOutline />
+          </IconButton>
+        </Tooltip>
       )}
       {onEdit && (
-        <IconButton onClick={onEdit} color="info">
-          <EditOutlinedIcon />
-        </IconButton>
+        <Tooltip title="ویرایش">
+          <IconButton onClick={onEdit} color="info">
+            <EditOutlinedIcon />
+          </IconButton>
+        </Tooltip>
       )}
       {onDelete && (
-        <IconButton onClick={onDelete} color="error">
-          <DeleteOutlineOutlinedIcon />
-        </IconButton>
+        <Tooltip title="حذف">
+          <IconButton onClick={onDelete} color="error">
+            <DeleteOutlineOutlinedIcon />
+          </IconButton>
+        </Tooltip>
       )}
     </Box>
   );
