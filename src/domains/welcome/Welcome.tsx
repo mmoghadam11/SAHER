9103ffbc2e82b4df91ab0ccc2ba3512dcaeaf001
mahdiyar_IACPA <mahyar_moghadam11@yahoo.ca>
2025-09-22 16:27:17 +0330

    carousel Baisics -a

--- src/domains/welcome/Welcome.tsx
+++ src/domains/welcome/Welcome.tsx
@@ -1,14 +1,51 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 // import { useKeycloak } from "@react-keycloak/web";
 import TavanaSpinner from "components/spinner/TavanaSpinner";
 import { useAuth } from "hooks/useAuth";
 import Layout from "components/layout/Layout";
 import { Outlet } from "react-router-dom";
-import { Typography } from "@mui/material";
-
+import { Grid, Typography } from "@mui/material";
+import Carousel from "components/Carousel/Carousel";
+interface CarouselItem {
+  title: string;
+  image: string;
+  description: string;
+}
+const carouselItems: CarouselItem[] = [
+  {
+    title: "عنوان اول",
+    image: "https://picsum.photos/800/400?random=1",
+    description: "توضیحات مربوط به آیتم اول",
+  },
+  {
+    title: "عنوان دوم",
+    image: "https://picsum.photos/800/400?random=2",
+    description: "توضیحات مربوط به آیتم دوم",
+  },
+  {
+    title: "عنوان سوم",
+    image: "https://picsum.photos/800/400?random=3",
+    description: "توضیحات مربوط به آیتم سوم",
+  },
+];
 function Welcome() {
-    const Auth = useAuth();
-    return<Typography>خوش آمدید</Typography>;
+  const Auth = useAuth();
+  return (
+    <Grid container justifyContent={"center"}>
+      <Grid item md={11}>
+        <Typography variant="h6">خوش آمدید</Typography>
+      </Grid>
+      {/* <Grid item md={4}>
+        <Carousel
+          items={carouselItems}
+          autoPlay={true}
+          interval={5000}
+          height={300}
+        />
+      </Grid> */}
+
+    </Grid>
+  );
 }
 
 export default Welcome;
