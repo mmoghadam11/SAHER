10bd81c93e896663926f023faa88e6344cad8041
mahdiyar_IACPA <mahyar_moghadam11@yahoo.ca>
2025-12-24 16:51:24 +0330

    colors

--- src/components/table/TableActions.tsx
+++ src/components/table/TableActions.tsx
@@ -3,12 +3,22 @@ import { Box, Grid, IconButton, Tooltip } from "@mui/material";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
-import { AddCircleOutline, ManageAccountsOutlined } from "@mui/icons-material";
+import { AddCircleOutline, AutoStories, ManageAccountsOutlined } from "@mui/icons-material";
 
 type Props = {
   onEdit?: (record: any) => void;
   onDelete?: (record: any) => void;
   onView?: (record: any) => void;
+  onEditF?: {
+    function: (record: any) => void;
+    title?: string;
+    icon?: any;
+  };
+  onViewF?: {
+    function: (record: any) => void;
+    title?: string;
+    icon?: any;
+  };
   onAdd?: {
     function: (record: any) => void;
     title?: string;
@@ -19,14 +29,22 @@ type Props = {
     title?: string;
     icon?: any;
   };
+  onRead?: {
+    function: (record: any) => void;
+    title?: string;
+    icon?: any;
+  };
 };
 
 const TableActions: React.FC<Props> = ({
   onEdit,
   onDelete,
   onView,
+  onEditF,
+  onViewF,
   onAdd,
   onManage,
+  onRead,
 }) => {
   return (
     <Grid container justifyContent="center">
@@ -39,11 +57,29 @@ const TableActions: React.FC<Props> = ({
           </Tooltip>
         </Grid>
       )}
+      {onViewF && (
+        <Grid item>
+          <Tooltip title={onViewF.title??"مشاهده"}>
+            <IconButton onClick={onViewF.function} color="primary">
+              {onViewF?.icon || <AddCircleOutline />}
+            </IconButton>
+          </Tooltip>
+        </Grid>
+      )}
       {onEdit && (
         <Grid item>
           <Tooltip title="ویرایش">
             <IconButton onClick={onEdit} color="info">
               <EditOutlinedIcon />
+            </IconButton>
+          </Tooltip>
+        </Grid>
+      )}
+      {onEditF && (
+        <Grid item>
+          <Tooltip title={onEditF.title??"ویرایش"}>
+            <IconButton onClick={onEditF.function} color="primary">
+              {onEditF?.icon || <EditOutlinedIcon />}
             </IconButton>
           </Tooltip>
         </Grid>
@@ -62,6 +98,15 @@ const TableActions: React.FC<Props> = ({
           <Tooltip title={onManage.title??"جزئیات"}>
             <IconButton onClick={onManage.function} color="primary">
               {onManage?.icon || <ManageAccountsOutlined />}
+            </IconButton>
+          </Tooltip>
+        </Grid>
+      )}
+      {onRead && (
+        <Grid item>
+          <Tooltip title={onRead.title??"مشاهده"}>
+            <IconButton onClick={onRead.function} color="primary">
+              {onRead?.icon || <AutoStories />}
             </IconButton>
           </Tooltip>
         </Grid>
