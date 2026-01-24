586d0c0fb7fbb3c926c73d10e4df3da99b421c87
mahdiyar_IACPA <mahyar_moghadam11@yahoo.ca>
2026-01-24 16:04:57 +0330

    officePhone & personnelEmail -R

--- src/domains/firmAdmin/FirmHiredAccountant/forms/AccountantAssignmentFormItems.tsx
+++ src/domains/firmAdmin/FirmHiredAccountant/forms/AccountantAssignmentFormItems.tsx
@@ -30,30 +30,30 @@ export const AccountantAssignmentFormItems = (
       value: "",
     },
   },
-  {
-    name: "officePhone",
-    inputType: "text",
-    label: "تلفن دفتر",
-    size: { md: 6 },
-    rules: { 
-      pattern: {
-        value: /^[0-9]{8,11}$/,
-        message: "شماره تلفن باید بین 8 تا 11 رقم باشد"
-      }
-    },
-  },
-  {
-    name: "personnelEmail",
-    inputType: "text",
-    label: "ایمیل",
-    size: { md: 6 },
-    rules: { 
-      pattern: {
-        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-        message: "فرمت ایمیل نامعتبر است"
-      }
-    },
-  },
+  // {
+  //   name: "officePhone",
+  //   inputType: "text",
+  //   label: "تلفن دفتر",
+  //   size: { md: 6 },
+  //   rules: { 
+  //     pattern: {
+  //       value: /^[0-9]{8,11}$/,
+  //       message: "شماره تلفن باید بین 8 تا 11 رقم باشد"
+  //     }
+  //   },
+  // },
+  // {
+  //   name: "personnelEmail",
+  //   inputType: "text",
+  //   label: "ایمیل",
+  //   size: { md: 6 },
+  //   rules: { 
+  //     pattern: {
+  //       value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+  //       message: "فرمت ایمیل نامعتبر است"
+  //     }
+  //   },
+  // },
 ];
 
 
